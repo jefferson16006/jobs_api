4224a754a136d5e2936b53217868072214f7e265
jefferson16006 <jeffersonchukwu2006@gmail.com>
2025-01-31 08:20:35 +0100

    Modified & updated

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
 const express = require('express')
 const app = express()
 const authRouter = require('./routes/auth')
@@ -9,7 +15,18 @@ const notFoundMiddleware = require('./middleware/not-found')
 require('dotenv').config()
 require('express-async-errors')
 
+app.set('trust-proxy', 1)
+app.use(rateLimiter({
+    windowMswindowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+}))
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
